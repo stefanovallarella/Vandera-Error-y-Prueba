88b48726c6ff79d36cd45e8cb46d64a96b2b19a1
stefanovallarella <stefanovallarella@gmail.com>
2020-11-27 16:33:21 -0300

    Se reubica carpeta Design con imagen de ejemplo(tapa completa)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import fotoPrueba from './design/erroryprueba-tapa-completa.jpg'
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h1>VANDERA</h1>
       <hr/>
       <h3>Error y prueba</h3>
+      <img className="imagenPrueba" src={fotoPrueba} alt="Portada" />
     </React.Fragment>
   );
 }
