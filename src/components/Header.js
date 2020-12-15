6d43cbefaaa1718cc195fa206c8d98039963f22c
stefanovallarella <stefanovallarella@gmail.com>
2020-12-15 19:23:51 -0300

    Se añade carpeta img con logo.svg y hoja de estilo por componente. Avances en el Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import '../styles/header.css';
+import LogoVandera from '../design/img/vanderalogo.svg';
 
 
 function Header() {
 
     return (
-            <div>
-                <p>HEADER</p>
+            <div class='header-container'>
+                <div className="logo-container">
+                    <img className="header-logo" src={LogoVandera} alt="logo vandera"/>
+                </div>
+
+                <div className="link-container">
+                    <p class='pepito'>HEADER</p>
+                </div>
             </div>
     )   
-
 }
-
 
 export default Header;
 
