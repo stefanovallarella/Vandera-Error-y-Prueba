21bcdc5dc7e3bebb8a6135b19c339a23d5af7c4b
stefanovallarella <stefanovallarella@gmail.com>
2020-12-17 22:48:51 -0300

    Avances en el header. Se agregan íconos desde FontAwesome

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,29 @@ function Header() {
                     <img className="header-logo" src={LogoVandera} alt="logo vandera"/>
                 </div>
 
-                <div className="link-container">
-                    <p class='pepito'>HEADER</p>
+
+                <div className="link-redes-container">
+                    <div className="link-container">
+                        <a href="#">SOBRE EL DISCO</a>
+                        <a href="#">CONTACTO</a>
+                    </div>
+
+                    <div className="redes-container">
+                        <a href="#">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>    
+                        <a href="#">
+                            <i class="fab fa-twitter"></i>
+                        </a>   
+                        <a href="#">
+                            <i class="fab fa-instagram"></i>
+                        </a>   
+                        <a href="#">
+                            <i class="fab fa-youtube"></i>
+                        </a>   
+                    </div>
                 </div>
+
             </div>
     )   
 }
