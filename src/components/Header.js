3c1ed248ad09e1d7447dbd4a70b51a5357995e38
sebaferrari23 <sebaferrari23@gmail.com>
2020-12-21 21:46:49 -0300

    refactor: header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,26 @@
-import React from 'react';
-import '../styles/header.css';
-import LogoVandera from '../design/img/vanderalogo.svg';
+import LogoVandera from './LogoVandera.js';
+import Social from './Social.js';
 
-
-function Header() {
-
+const Header = () => {
     return (
-            <div class='header-container'>
-                <div className="logo-container">
-                    <img className="header-logo" src={LogoVandera} alt="logo vandera"/>
-                </div>
+        <header className='header'>
 
+            <div className="container">
 
-                <div className="link-redes-container">
-                    <div className="link-container">
-                        <a href="#">SOBRE EL DISCO</a>
-                        <a href="#">CONTACTO</a>
-                    </div>
+                <LogoVandera />
 
-                    <div className="redes-container">
-                        <a href="#">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>    
-                        <a href="#">
-                            <i class="fab fa-twitter"></i>
-                        </a>   
-                        <a href="#">
-                            <i class="fab fa-instagram"></i>
-                        </a>   
-                        <a href="#">
-                            <i class="fab fa-youtube"></i>
-                        </a>   
-                    </div>
+                <div className="header__menu">
+
+                    <nav className="header__nav">
+                        <a href="/">Sobre el disco</a>
+                        <a href="/">Contacto</a>
+                    </nav>
+
+                    <Social />
+
                 </div>
             </div>
+        </header>
     )   
 }
 
---
+++ src/components/Social.js
@@ -0,0 +1,19 @@
+const Social = () => {
+    return (
+        <div className="social">
+            <a href="https://facebook.com/vanderaw" target="_blank" rel="nofollow noreferrer">
+                <i className="fab fa-facebook-f"></i>
+            </a>    
+            <a href="https://twitter.com/vanderacarlos" target="_blank" rel="nofollow noreferrer">
+                <i className="fab fa-twitter"></i>
+            </a>   
+            <a href="https://instagram.com/vanderacarlos" target="_blank" rel="nofollow noreferrer">
+                <i className="fab fa-instagram"></i>
+            </a>   
+            <a href="https://youtube.com/channel/UCilRAmlq5LBcllREXS5lVJQ" target="_blank" rel="nofollow noreferrer">
+                <i className="fab fa-youtube"></i>
+            </a>   
+        </div>
+    )
+}
+export default Social
