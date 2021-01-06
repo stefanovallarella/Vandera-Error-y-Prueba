63da4f513137db684f6b88c833e6a5de0e62e7b2
sebaferrari23 <sebaferrari23@gmail.com>
2021-01-06 13:24:43 -0300

    Footer and About album styles

--- src/components/AboutAlbum.js
+++ src/components/AboutAlbum.js
@@ -1,8 +1,39 @@
-const AboutAlbum = () => {
+import { connect } from 'react-redux';
+import mainM from '../design/img-manchas/error-y-prueba-manchas.png';
+
+const AboutAlbum = ({actualSong}) => {
     return (
-        <div>
-            <p>SOBRE EL DISCO</p>
-        </div>
+        <section className="block block--about">
+            <div className="container">
+
+                <div className="gallery">
+                    {/* <a href="/" className="gallery__item" style={{background: 'red'}}></a>
+                    <a href="/" className="gallery__item" style={{background: 'yellow'}}></a>
+                    <a href="/" className="gallery__item" style={{background: 'yellow'}}></a>
+                    <a href="/" className="gallery__item" style={{background: 'red'}}></a>
+                    <a href="/" className="gallery__item" style={{background: 'blue'}}></a> */}
+                </div>
+                
+                <div className="block__content">
+                    <h3 className="title title--border">Sobre el disco</h3>
+                    <div className="block__txt">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut scelerisque turpis. Duis mauris augue Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut scelerisque turpis. Duis mauris augue.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut scelerisque turpis. Duis mauris augue Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="block__bg"
+                style={{
+                backgroundImage: `url(${actualSong.mainSectionBg ? actualSong.mainSectionBg : mainM })`
+                }}
+            ></div>
+        </section>
     )   
 }
-export default AboutAlbum;
+
+const mapStateToProps = state => ({
+    actualSong: state.songs.actualSong
+});
+export default connect(mapStateToProps)(AboutAlbum);
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,47 @@
 const Footer = () => {
     return (
-        <div>
-            <p>FOOTER</p>
-        </div>
+        <footer className="block block--footer block--gradient">
+            <div className="container">
+                <div className="block__content">
+                    <h4 class="title title--border">Contacto</h4>
+                    <ul className="block__list">
+                        <li>
+                            <i className="fas fa-envelope"></i>
+                            <a href="mailto:info@vandera.com.ar">info@vandera.com.ar</a>
+                        </li>
+                        <li>
+                            <i className="fas fa-envelope"></i>
+                            <a href="mailto:info@vandera.com.ar">info@vandera.com.ar</a>
+                        </li>
+                        <li>
+                            <i className="fas fa-phone"></i>
+                            <a href="tel:+543415532667">+54 341 5532667</a>
+                        </li>
+                        <li>
+                            <i className="fas fa-phone"></i>
+                            <a href="tel:+543415532667">+54 341 5532667</a>
+                        </li>
+                    </ul>
+                    <div className="block__bottom">
+                        <p>Copyright © 2021 Vandera</p>
+                        <div className="social social--white">
+                            <a href="https://facebook.com/vanderaw" target="_blank" rel="nofollow noreferrer">
+                                <i className="fab fa-facebook-f"></i>
+                            </a>    
+                            <a href="https://twitter.com/vanderacarlos" target="_blank" rel="nofollow noreferrer">
+                                <i className="fab fa-twitter"></i>
+                            </a>   
+                            <a href="https://instagram.com/vanderacarlos" target="_blank" rel="nofollow noreferrer">
+                                <i className="fab fa-instagram"></i>
+                            </a>   
+                            <a href="https://youtube.com/channel/UCilRAmlq5LBcllREXS5lVJQ" target="_blank" rel="nofollow noreferrer">
+                                <i className="fab fa-youtube"></i>
+                            </a>   
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )   
 }
 export default Footer;
