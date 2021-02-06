38e9cd98389b1ec94e3ded0519f2a587d37eaa80
sebaferrari23 <sebaferrari23@gmail.com>
2021-02-06 01:34:21 -0300

    Gallery about

--- src/components/AboutAlbum.js
+++ src/components/AboutAlbum.js
@@ -1,14 +1,12 @@
+
+import GalleryWrapper from '../components/Gallery'
 import { connect } from 'react-redux';
 import mainM from '../assets/images/img-manchas/error-y-prueba-manchas.png';
-import AboutImage1 from '../assets/images/about/2.jpeg';
-import AboutImage2 from '../assets/images/about/21.png';
-import AboutImage3 from '../assets/images/about/22.jpeg';
-import AboutImage4 from '../assets/images/about/23.png';
-import AboutImage5 from '../assets/images/about/pc.jpg';
+import VanderaFirma from '../assets/images/img/firma-vandera.png';
 
 const AboutAlbum = ({actualSong}) => {
     return (
-        <section className="block block--about">
+        <section id="album" className="block block--about">
             <div className="container">
 
                 <div className="block__content">
@@ -17,25 +15,13 @@ const AboutAlbum = ({actualSong}) => {
                         <p>Es un disco realizado de una forma mas natural, buscando la canción en la primera o segunda toma de Vox y dejando que la misma música sugiera los arreglos y la instrumentación. Como si se estuviera tocando en vivo y sin tiempo de revisar demasiado. Todos las versiones de las canciones son las primeras ideas de las mismas. Todo estas decisiones fueron dandole al álbum un color y una textura particular. Esto no lo hace ni mejor ni peor, solo lo describe en su gestación.</p>
                         <p>"Errar es humano" se dice que es intrínseco a la naturaleza humana el equivocarse, por lo que hay que aceptar los errores, y aprender de ellos para evitar que se repitan. En la música el error es el acierto y viceversa</p>
                     </div>
+                    <div className="block__about">
+                        <img src={VanderaFirma} alt=""/>
+                        <p>Carlos Vandera</p>
+                    </div>
                 </div>
 
-                <div className="gallery">
-                    <a href={AboutImage1} className="gallery__item">
-                        <img src={AboutImage1} alt=""/>
-                    </a>
-                    <a href={AboutImage2} className="gallery__item">
-                        <img src={AboutImage2} alt=""/>
-                    </a>
-                    <a href={AboutImage3} className="gallery__item">
-                        <img src={AboutImage3} alt=""/>
-                    </a>
-                    <a href={AboutImage4} className="gallery__item">
-                        <img src={AboutImage4} alt=""/>
-                    </a>
-                    <a href={AboutImage5} className="gallery__item">
-                        <img src={AboutImage5} alt=""/>
-                    </a>
-                </div>
+                <GalleryWrapper />
 
             </div>
 
