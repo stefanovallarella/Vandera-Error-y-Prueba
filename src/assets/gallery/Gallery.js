f498718d4e349d92ae0e5aad5a3fce8ded813c77
stefanovallarella <stefanovallarella@gmail.com>
2021-03-09 20:38:45 -0300

    Videos and images left added.

--- src/assets/gallery/Gallery.js
+++ src/assets/gallery/Gallery.js
@@ -6,6 +6,7 @@ import AboutVideo5 from '../images/about/5.m4v';
 import AboutVideo6 from '../images/about/6.m4v';
 import AboutVideo7 from '../images/about/7.m4v';
 import AboutVideo8 from '../images/about/8.m4v';
+import AboutVideo9 from '../images/about/9.m4v';
 
 import AboutThumbVideo1 from '../images/about/1-thumbnail.jpg';
 import AboutThumbVideo2 from '../images/about/2-thumbnail.jpg';
@@ -21,6 +22,17 @@ import AboutImage2 from '../images/about/2.jpg';
 import AboutImage3 from '../images/about/3.jpg';
 import AboutImage4 from '../images/about/4.jpg';
 import AboutImage5 from '../images/about/5.jpg';
+import AboutImage6 from '../images/about/6.jpg';
+import AboutImage7 from '../images/about/7.jpg';
+import AboutImage8 from '../images/about/8.jpg';
+import AboutImage9 from '../images/about/9.jpg';
+import AboutImage10 from '../images/about/10.jpg';
+import AboutImage11 from '../images/about/11.jpg';
+import AboutImage12 from '../images/about/12.jpg';
+import AboutImage13 from '../images/about/13.jpg';
+import AboutImage14 from '../images/about/14.jpg';
+import AboutImage15 from '../images/about/15.jpg';
+
 
 const gallery = [
     { media: AboutVideo3, thumb: AboutThumbVideo3, type: 'video' },
@@ -31,11 +43,22 @@ const gallery = [
     { media: AboutVideo6, thumb: AboutThumbVideo6, type: 'video' },
     { media: AboutVideo7, thumb: AboutThumbVideo7, type: 'video' },
     { media: AboutVideo8, thumb: AboutThumbVideo8, type: 'video' },
+    { media: AboutVideo9, thumb: AboutThumbVideo8, type: 'video' },
     { media: AboutImage1, type: 'image' },
     { media: AboutImage2, type: 'image' },
     { media: AboutImage3, type: 'image' },
     { media: AboutImage4, type: 'image' },
     { media: AboutImage5, type: 'image' },
+    { media: AboutImage6, type: 'image' },
+    { media: AboutImage7, type: 'image' },
+    { media: AboutImage8, type: 'image' },
+    { media: AboutImage9, type: 'image' },
+    { media: AboutImage10, type: 'image' },
+    { media: AboutImage11, type: 'image' },
+    { media: AboutImage12, type: 'image' },
+    { media: AboutImage13, type: 'image' },
+    { media: AboutImage14, type: 'image' },
+    { media: AboutImage15, type: 'image' }
 ]
 
 export default gallery;
